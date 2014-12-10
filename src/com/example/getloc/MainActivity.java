42c6a6c73d83a94531d9945840d70b2d72960db2
sumedhm <sumedhm18@gmail.com>
2014-12-10 06:31:14 +0530

    Getting old and new location and comparing

--- src/com/example/getloc/MainActivity.java
+++ src/com/example/getloc/MainActivity.java
@@ -24,19 +24,17 @@ public class MainActivity extends Activity implements
     LocationClient mLocationClient;
     Location mCurrentLocation;
     LocationRequest mLocationRequest;
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
      
-    TextView txtLong,txtLat;
+    TextView txtLong,txtLat,txtLong2,txtLat2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 1. setContnetView
         setContentView(R.layout.activity_main);
          
-        // 2. get reference to TextView
         txtLong = (TextView) findViewById(R.id.txtLong);
         txtLat = (TextView) findViewById(R.id.txtLat);
  
-        // 3. create LocationClient
         mLocationClient = new LocationClient(this, this, this);
  
         // 4. create & set LocationRequest for Location update
@@ -75,6 +73,8 @@ public class MainActivity extends Activity implements
          
         if(mLocationClient != null)
             mLocationClient.requestLocationUpdates(mLocationRequest,  this);
+        
+        
  
         Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
          
@@ -113,5 +113,45 @@ public class MainActivity extends Activity implements
  
         txtLong.setText(mCurrentLocation.getLongitude()+"");
     }
-     
+    
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            return true;
+        }
+
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        if (isSignificantlyNewer) {
+            return true;
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+          return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
 }
